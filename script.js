27c629587a29a4c9cf7103dca3c4763e5da5d9c0
teke <mutau85@gmail.com>
2022-09-08 19:09:47 +0200

    Finish modal_window-popup

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const projects = [
       'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. ",
-    skills: ['html', 'css', 'javascript'],
+    skills: ['html', 'css', 'javascript', 'github', 'ruby', 'Bootstraps'],
     liveLink: 'https://www.canopy.cr/tonic',
     sourceLink: 'https://www.canopy.cr/tonic',
   },
@@ -116,50 +116,71 @@ myContent.innerHTML = showInHtml;
 /* View Pop Up */
 const popUpDetails = projects.map(
   (project) => `<section class="popup">
-    <div class="popup-header">
-      <h2 class="popup-project-title" id="popup-project-title">${project.title}</h2>
-      <span id="cl" class="close">&times;</span>
-    </div>
-    <div class="popup-project-info" id="project-info"></div>
-    <div id="project-image-holder">
-      <img
-        src=${project.screenshot}
-        alt="Tonic project screenshot"
-        class="popup-project-image"
-        id="popup-project-image"
-      />
-    </div>
-    <div class="popup-project-details">
-      <div class="column-left">
-        <p class="popup-project-description" id="project-description">${project.description}</p>
-      </div>
-      <div class="column-right">
-        <div class="popup-project-stack" id="popup-project-stack">
-        <ul class="btn-container-flex">
-          <li class="btn">
-            <button class="btn-sm" type="button">${project.skills[0]}</button>
-          </li>
-          <li class="btn">
-            <button class="btn-md" type="button">${project.skills[1]}</button>
-          </li>
-          <li class="btn">
-            <button class="btn-lg" type="button">${project.skills[2]}</button>
-          </li>
-        </ul>
-        </div>
-        
-        <div class="popup-project-buttons">
-          <button type="button" id="see-live-bt" class="newBtn" onclick="">
-            See Live
+<div class="popup-header">
+  <h2 class="popup-project-title" id="popup-project-title">
+    ${project.title}
+  </h2>
+  <span id="cl" class="close">&times;</span>
+</div>
+<div class="popup-project-info" id="project-info"></div>
+<div id="project-image-holder">
+  <img
+    src="${project.screenshot}"
+    alt="Tonic project screenshot"
+    class="popup-project-image"
+    id="popup-project-image"
+  />
+</div>
+<div class="popup-project-details">
+  <div class="column-left">
+    <p class="popup-project-description" id="project-description">
+      ${project.description}
+    </p>
+  </div>
+  <div class="column-right">
+    <div class="popup-project-stack" id="popup-project-stack">
+      <ul class="btn-container-flex">
+        <li class="btn">
+          <button class="btn-sm" type="button">
+            ${project.skills[0]}
           </button>
-          <button type="button" id="see-source-btn" class="newBtn" onclick="">
-            See Source
+          <button class="btn-sm" type="button">
+            ${projects[0].skills[3]}
           </button>
-        </div>
-      </div>
+        </li>
+        <li class="btn">
+          <button class="btn-md" type="button">
+            ${project.skills[1]}
+          </button>
+          <button class="btn-md" type="button">
+          ${projects[0].skills[4]}
+          </button>
+        </li>
+        <li class="btn">
+          <button class="btn-lg" type="button">
+            ${project.skills[2]}
+          </button>
+          <button class="btn-lg" type="button">
+          ${projects[0].skills[5]}
+          </button>
+        </li>
+      </ul>
     </div>
-   </section>`
+
+    <div class="popup-project-buttons">
+      <button type="button" id="see-live-bt" class="newBtn" onclick="">
+        See Live
+      </button>
+      <button type="button" id="see-source-btn" class="newBtn" onclick="">
+        See Source
+      </button>
+    </div>
+  </div>
+</div>
+</section>`
 );
+
+console.log(projects[0].skills[3]);
 /* Pop Up Menu */
 const popup = document.getElementById('popup');
 
@@ -189,72 +210,5 @@ window.onclick = (event) => {
     popup.style.display = 'none';
   }
 };
+
 // Form Validation
-
-const form = document.getElementById('form');
-const emailError = document.querySelector('span.error');
-
-const isEmailValid = (email) => {
-  const regex = /^[a-z]+@[a-z0-9-]+\.[a-z0-9-.]+$/;
-  return regex.test(email.value);
-};
-
-const checkEmail = (email) => {
-  let valid = true;
-  if (!isEmailValid(email)) {
-    valid = false;
-  }
-  return valid;
-};
-
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-
-  const { email } = form.elements;
-
-  if (!checkEmail(email)) {
-    email.setCustomValidity(
-      'Please enter a valid email address and in lowercase letters.'
-    );
-    emailError.textContent =
-      'Please enter a valid email address and in lowercase letters.';
-  } else {
-    email.setCustomValidity('');
-    emailError.textContent = '';
-    emailError.className = 'error';
-    form.submit();
-  }
-});
-
-const fullname = document.getElementById('fullname');
-const email = document.getElementById('email');
-const message = document.getElementById('message');
-
-const store = () => {
-  const inputs = {
-    fullname: fullname.value,
-    email: email.value,
-    message: message.value,
-  };
-  localStorage.setItem('formInput', JSON.stringify(inputs));
-};
-
-fullname.addEventListener('change', () => {
-  store();
-});
-
-email.addEventListener('change', () => {
-  store();
-});
-
-message.addEventListener('change', () => {
-  store();
-});
-
-const populateForm = () => {
-  const storage = localStorage.getItem('formInput');
-  fullname.value = JSON.parse(storage).fullname;
-  email.value = JSON.parse(storage).email;
-  message.value = JSON.parse(storage).message;
-};
-populateForm();
