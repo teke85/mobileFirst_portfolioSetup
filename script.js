1e42f904558aa865cad2690e6e7870f447d5035b
teke <mutau85@gmail.com>
2022-09-09 13:42:46 +0200

    add  bug

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const overlay = document.querySelector('.mobile-overlay');
 function mytoggleMenu() {
-  overlay.classList.toggle('hide');
+  overlay.classList.toggle('');
 }
 mytoggleMenu();
+
 const projects = [
   {
     id: 1,
@@ -111,7 +112,7 @@ const showInHtml = projects
       </div>
      </div>`,
   )
-  .join('');
+  .('');
 myContent.innerHTML = showInHtml;
 /* View Pop Up */
 const popUpDetails = projects.map(
@@ -210,3 +211,32 @@ window.onclick = (event) => {
 };
 
 // Form Validation
+const form = document.getElementById('form');
+const emailError = document.querySelector('span.error');
+
+const isEmailValid = (email) => {
+  const regex = /^[a-z]+@[a-z0-9-]+\.[a-z0-9-.]+$/;
+  return regex.test(email.value);
+};
+
+const checkEmail = (email) => {
+  let valid = true;
+  if (!isEmailValid(email)) {
+    valid = false;
+  }
+  return valid;
+};
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const { email } = form.elements;
+  if (!checkEmail(email)) {
+    email.setCustomValidity('Please enter a valid email address and in lowercase letters.');
+    emailError.textContent = 'Please enter a valid email address and in lowercase letters.';
+  } else {
+    email.setCustomValidity('');
+    emailError.textContent = '';
+    emailError.className = 'error';
+    form.submit();
+  }
+});
